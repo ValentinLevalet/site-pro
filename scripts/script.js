9119dff2d392b6acf9777b63008abfb5730e4d7a
Valentin Levalet <valentin.levalet@live.fr>
2017-02-27 22:45:45 +0100

    Ajout icones sociales

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+var a = ["lightgrey", "lightblue", "lightyellow", "lightcyan"];
+var i = 0;
+var b = document.getElementsByTagName('body');
+b = b[0];
+
+//console.log(b)
+function init(){
+  setTimeout(function(e){
+    //alert(a[i]);
+    if(i == 3)i = 0;
+    b.style.background = a[i];
+    i++;
+    init();
+  },10000);
+};
+init();
+
+var cont = document.getElementsByClassName('icone-contact');
+var form = document.getElementsByClassName('div-form');
+console.log(form)
+cont[0].addEventListener('click', function(){
+    form[0].classList.toggle('hidden');
+})
